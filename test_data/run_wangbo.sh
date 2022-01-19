fd928d0bc8aa53299d80ab02e500d4d4c835a3b7
CrazyHsu <crazyhsu9527@gmail.com>
2022-01-19 22:57:18 +0800

    add running pipeline

---
+++ test_data/run_wangbo.sh
@@ -0,0 +1,10 @@
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py preproc -c /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py mapping -c /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg -c -jcs 2
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py collapse -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py refine -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg -refine
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py find_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py visual_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg -g Zm00001d050245
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py rank_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py diff_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg -d /data/CrazyHsu_data/Projects/isoseq/iFLAS_toolkit_20210510/iFLAS_toolkit/test_data/wangbo_compCond.txt
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py go -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg -tg targetGene1.txt,targetGene2.txt,targetGene3.txt -bg /data/CrazyHsu_data/Projects/isoseq/iFLAS_toolkit_20210510/iFLAS_toolkit/test_data/gene2go.txt -s sample1,sample2,sample3
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py report -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo.cfg
---
+++ test_data/run_wanglab.sh
@@ -0,0 +1,9 @@
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py preproc -c /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py mapping -c /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg -c -jcs 2
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py collapse -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py refine -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg -refine
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py find_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py visual_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg -g Zm00001d050245
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py rank_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py go -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg -tg targetGene1.txt,targetGene2.txt,targetGene3.txt -bg /data/CrazyHsu_data/Projects/isoseq/iFLAS_toolkit_20210510/iFLAS_toolkit/test_data/gene2go.txt -s sample1,sample2,sample3
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py report -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wanglab.cfg
