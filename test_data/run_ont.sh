e99371ab2afe16f13982b65f4e3201d0a7fbd75a
CrazyHsu <crazyhsu9527@gmail.com>
2022-01-28 02:19:19 +0800

    update

--- test_data/run_ont.sh
+++ test_data/run_ont.sh
@@ -7,4 +7,4 @@ python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py visual_as -cfg 
 python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py rank_iso -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_ont.cfg
 python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py palen_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_ont.cfg
 python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py go -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_ont.cfg -tg targetGene1.txt,targetGene2.txt,targetGene3.txt -bg /data/CrazyHsu_data/Genomes/maize/Zea_mays.AGPv4.50/gene2goId.txt -s sample1,sample2,sample3
-python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py report -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_ont.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py report -basic -asp -geneStruc -palen -html -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_ont.cfg
--- test_data/run_wangbo_hybrid.sh
+++ test_data/run_wangbo_hybrid.sh
@@ -7,4 +7,4 @@ python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py visual_as -cfg 
 python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py rank_iso -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo_hybrid.cfg
 python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py allele_as -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo_hybrid.cfg
 #python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py go -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo_hybrid.cfg -tg test_data/targetGene1.txt,test_data/targetGene2.txt,test_data/targetGene3.txt -bg /data/CrazyHsu_data/Genomes/maize/Zea_mays.AGPv4.50/gene2goId.txt -s sample1,sample2,sample3 -filterby pvalue -cutoff 0.1
-python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py report -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo_hybrid.cfg
+python /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/iflas.py report -basic -asp -geneStruc -asas -html -cfg /data/CrazyHsu_data/Projects/isoseq/iFLAS_simple/test_data/test_pacbio_wangbo_hybrid.cfg
